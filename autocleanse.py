ec7b3e032ce554bfa910395a085a207d5cd02464
Chinmay Shet <chishxd@gmail.com>
2025-06-17 14:18:11 +0530

    Implemented Data Cleaning Function

--- autocleanse.py
+++ autocleanse.py
@@ -8,7 +8,7 @@ def load_csv(data : str) -> pd.DataFrame:
         data (str): File path to the CSV file.
 
     Returns:
-        pd.DataFrame: DataFrame parsed from the CSV file.
+        df (pd.DataFrame): DataFrame parsed from the CSV file.
 
     Raises:
         FileNotFoundError: If the file path is incorrect or the file does not exist.
@@ -35,14 +35,15 @@ def profile_dataframe(df: pd.DataFrame):
         df (pd.DataFrame): The input Pandas DataFrame.
 
     Returns:
-        TBD: Currently returns [TODO: A CSV/JSON file with profiling data for API integration].
+        STDOUT (str): Output is shown to terminal.
 
     Raises:
-        None
+        Nothing
     """
 
     print("--- Profiling Data ---")
-
+    print("First 5 rows in the DataFrame: ")
+    print(df.head())
     print(f"Shape of Dataframe(rows, columns):{df.shape}\n") #Prints rows and columns in a Dataframe
     print("Name Of Each Column:")
     for column in df.columns: #Uses For loop to go through each column in the Index of all columns in the dataframe and print them
@@ -75,11 +76,47 @@ def profile_dataframe(df: pd.DataFrame):
     print()
     print("--- End Of Profiling ---")
 
+#This is our work of Day 2, We are cleaning DataFrame so that we can train the data without any errors.
+def handle_missing_data(df : pd.DataFrame) -> pd.DataFrame:
+    '''
+    Function to clean the provided data by:
+    - Dropping Columns with too many Null values
+    - Imputing data in barely any empty values
+
+    Args:
+        df (pd.DataFrame): A Panda Dataframe
+
+    Returns:
+        df (pd.DataFrame): The cleaned DataFrame with missing values handled and specified columns dropped.
+    '''
+    nullCols = [] #To store Columns with more than 60% Null Values.
+    for column in df.columns:
+        if df[column].isnull().sum() / len(df) * 100 > 60.00 : #Same comparision from the profiling_dataframe()
+            nullCols.append(column)
+
+        else:
+            #Check if the percentage of unique values in the DataFrame is less than 5%, if yes fill the categorical values from modes
+            if df[column].nunique()/len(df) * 100 < 5 :
+                df[column] = df[column].fillna(df[column].mode().iloc[0]) #use mode when imputing non-numerical and categorical data
+            else:
+                #Check if the column is numeric, if yes we can use Median to fill null values.
+                if pd.api.types.is_numeric_dtype(df[column]):
+                    df[column] = df[column].fillna(df[column].median()) #use median when imputing numerical data
+                else:
+                    df[column] = df[column].fillna('Unknown')
+    
+    df = df.drop(columns=nullCols) #To delete the columns from listed earlier in the funtion.
+    
+    return df
+
+
 #this part of the code let's us to check code in a seperate part when we are working in a modular ecosystem.
 #When this code is imported into other program, the code won't directly run, but only desired functions will run.
 if __name__ == "__main__":
         df = load_csv("data/train.csv")
         if df is not None:
             profile_dataframe(df)
+            new_df = handle_missing_data(df)
+            profile_dataframe(new_df)
         else:
             print("Could not load File properly")
