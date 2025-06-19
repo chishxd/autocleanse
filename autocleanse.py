116da03294c83b57de0ae2e676b239cbecc0ae79
Chinmay Shet <chishxd@gmail.com>
2025-06-19 15:09:27 +0530

    Added classifyAPI.py

--- autocleanse.py
+++ autocleanse.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import classifyapi
 
 def load_csv(data : str) -> pd.DataFrame:
     """
@@ -113,13 +114,17 @@ def handle_missing_data(df : pd.DataFrame) -> pd.DataFrame:
     return df
 
 
+def main():
+    # df = load_csv("data/Synthetic_data.csv") #Change file name to your dataset, I have provided train.csv in data/
+    df = classifyapi.load_from_s3("admin", "password", "http://127.0.0.1:9000", "train.csv")
+    if df is not None:
+        profile_dataframe(df)
+        new_df = handle_missing_data(df)
+        profile_dataframe(new_df)
+    else:
+        print("Could not load File properly")
+
 #this part of the code let's us to check code in a seperate part when we are working in a modular ecosystem.
 #When this code is imported into other program, the code won't directly run, but only desired functions will run.
 if __name__ == "__main__":
-        df = load_csv("data/Synthetic_data.csv") #Change file name to your dataset, I have provided train.csv in data/
-        if df is not None:
-            profile_dataframe(df)
-            new_df = handle_missing_data(df)
-            profile_dataframe(new_df)
-        else:
-            print("Could not load File properly")
+        main()
